db851c120bbbace7d45e4203ae08511a8022d99a
Hevitriniavo <hei.tantely@gmail.com>
2024-03-11 10:48:46 +0300

    feat: blog page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import {ReactNode} from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +15,15 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Header/>
+      {children}
+      <Footer/>
+      </body>
     </html>
   );
 }
